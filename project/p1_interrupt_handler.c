5a4b1dc343fa173167515fbaf2109e5123b4fbfe
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-25 21:30:00 -0700

    added comments

--- project/p1_interrupt_handler.c
+++ project/p1_interrupt_handler.c
@@ -1,19 +1,9 @@
 #include <msp430.h>
-
 #include "switches.h"
 
-
-
-//switch on P2
-
 void __interrupt_vec(PORT2_VECTOR) Port_2(){
-
-  if(P2IFG & SWITCHES){  //button cause an interrupt?
-
-    P2IFG &= ~SWITCHES;  // clear pending sw interrupt
-
-    switch_interrupt_handler(); // single handler for all switches
-
+  if(P2IFG & SWITCHES){         //if button cause interrupt
+    P2IFG &= ~SWITCHES;         //will clear sw interrupt
+    switch_interrupt_handler();      //handler for all switches
   }
-
 }
