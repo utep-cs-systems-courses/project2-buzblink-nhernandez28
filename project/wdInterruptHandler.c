4f931c2811adfa2df3fc0b43083a152f02ea0669
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-25 21:23:25 -0700

    modified variable names and methods being called from state machine file

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -1,65 +1,31 @@
 #include <msp430.h>
-
 #include "stateMachine.h"
-
-
 
 char switch_state_changed;
 
-
-
-void __interrupt_vecc(WDT_VECTOR) WDT(){ //250 interrupts/sec
-
-
-
+void __interrupt_vecc(WDT_VECTOR) WDT()
+{
+  //250 interrupts/sec
   static char blink_count = 0;
-
-  /*if(do_dim){
-
-    dimState++;
-
-    dimState %= 4
-
-    dim_advance();
-
-    }*/
-
   if(switch_state_changed == 4){
-
     switch(switch_state_changed){
-
     case 4:
-
       if(++count < 62){
-
-	dim25();
-
-      }else if(++count<125){
-
-	dim50();
-
+	dimAt25();
+      }else if(++count < 125){
+	dimAt50();
       }else if(++count == 250){
-
 	count = 0;
-
       }else{
-
-	dim75();
-
+	dimAt75();
       }
-
+      
       break;
-
     }
-
   }
 
   if(++blink_count == 125){
-
     state_advance();
-
     blink_count = 0;
-
   }
-
 }
