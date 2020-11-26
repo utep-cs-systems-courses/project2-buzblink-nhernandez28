1e8feaa83bc554c99bbd69cb54141097dbd0caae
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-25 21:32:05 -0700

    added comments

--- project/main.c
+++ project/main.c
@@ -1,31 +1,16 @@
 #include <msp430.h>
-
 #include "led.h"
-
 #include "switches.h"
-
 #include "buzzer.h"
-
 #include "libTimer.h"
 
-
-
 int main(void){
-
-  configureClocks(); //setup master oscillator, CPU & peripheral clocks
-
+  configureClocks();
   led_init();
-
   switch_init();
-
   buzzer_init();
-
-  enableWDTInterrupts(); //enable periodic interrupt
-
-  //  buzzer_set_period(1000); // start buzzing
-
-
-
-  or_sr(0x18); //CPU off, GIE on
+  enableWDTInterrupts(); //will enable interrupt
+  
+  or_sr(0x18); //CPU off / GIE on
 
 }
