8706fdf66ef0bf42929ef45317ceebb3c784940c
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-25 21:15:11 -0700

    modified the dim method calls

--- project/stateMachine.h
+++ project/stateMachine.h
@@ -1,27 +1,14 @@
 #ifndef stateMachine_included
-
 #define stateMachine_included
 
-
-
-//extern char tempo;
-
 void state_advance();
-
-
-
 void dim_leds(int x);
 
 char toggle_green();
-
 char toggle_red();
 
-void dim25();
-
-void dim50();
-
-void dim75();
-
-
+void dimAt25();
+void dimAt50();
+void dimAt75();
 
 #endif
