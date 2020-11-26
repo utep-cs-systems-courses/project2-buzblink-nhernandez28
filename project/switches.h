c679d40d6ec1987776c89e170550ef7e189857cc
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-25 21:17:53 -0700

    modified variable names being called

--- project/switches.h
+++ project/switches.h
@@ -1,31 +1,15 @@
 #ifndef switches_included
-
 #define switches_included
 
-
-
-#define SW1 BIT0 //switch1 is p2.0
-
-#define SW2 BIT1 //switch2 is p2.1
-
-#define SW3 BIT2 //switch3 is p2.2
-
-#define SW4 BIT3 //switch4 is p2.3
-
-#define SWITCHES (SW1 | SW2 | SW3 | SW4) //the four switches
-
-
+#define SW1 BIT0 //switch 1
+#define SW2 BIT1 //switch 2
+#define SW3 BIT2 //switch 3
+#define SW4 BIT3 //switch 4
+#define SWITCHES (SW1 | SW2 | SW3 | SW4) //switches
 
 void switch_init();
-
 void switch_interrupt_handler();
 
-
-
-extern char switch_state_down, switch_state_changed, s1, s2, s3, s4;
-
-//switch1, current_state; // effectively boolean
-
-
+extern char switch_state_down, switch_state_changed, s_1, s_2, s_3, s_4;
 
 #endif //included
