d28aa6f2d27cad0d63e4bf023bf0c528697668bb
Nancy Hernandez <nhernandez28@miners.utep.edu>
2020-11-27 23:15:53 -0700

    added couple new methods and fixed dim method bug

--- project/stateMachine.c
+++ project/stateMachine.c
@@ -5,47 +5,53 @@
 #include "switches.h"
 
 static char state_dim = 0;
+static char select_dim = 0;
 
+//red led will be only led blinking
 char toggle_red()
 {
   static char state = 0;
   switch(state){
   case 0:
-    red_on = 1;
+    g_on = 1;
     state = 1;
     break;
   case 1:
-    red_on = 0;
+    g_on = 0;
     state = 0;
     break;
   }
-
   return 1;
 }
 
+//green led will be only one blinking
 char toggle_green()
-{
-  char changed = 0;
-  
-  if(red_on){
-    green_on ^= 1;
-    changed = 1;
-  }
-
-  return changed;
-}
-
-char state_two()
 {
   static char state = 0;
   switch(state){
-
   case 0:
-    green_on = 1;
+    r_on = 1;
     state = 1;
     break;
   case 1:
-    green_on = 0;
+    r_on = 0;
+    state = 0;
+    break;
+  }
+  return 1;
+}
+
+//red led blinks
+char blink_red()
+{
+  static char state = 0;
+  switch(state){
+  case 0:
+    g_on = 1;
+    state = 1;
+    break;
+  case 1:
+    g_on = 0;
     state = 0;
     break;
   }
@@ -53,14 +59,113 @@ char state_two()
   return 1;
 }
 
-char state_three()
+//green led blinks
+char blink_green()
+{
+ static char state = 0;
+ switch(state){
+ case 0:
+   r_on = 1;
+   state = 1;
+   break;
+ case 1:
+   r_on = 0;
+   state = 0;
+   break;
+ }
+ 
+ return 1;
+}
+
+// 1/4
+void dimAt25()
+{
+  static char state_dim = 0;
+  switch(state_dim){
+  case 0:
+    r_on = 0;
+    state_dim = 1;
+    break;
+  case 1:
+    r_on = 0;
+    state_dim = 2;
+    break;
+  case 2:
+    r_on = 0;
+    state_dim = 3;
+  case 3:
+    r_on = 1;
+    state_dim = 0;
+    break;
+  }
+
+  changed_led = 1;
+  led_update();
+}
+
+// 1/2
+void dimAt50()
+{
+  static char state_dim = 0;
+  switch(state_dim){
+  case 0:
+    r_on = 1;
+    state_dim = 1;
+    break;
+  case 1:
+    r_on = 0;
+    state_dim = 2;
+    break;
+  case 2:
+    r_on = 1;
+    state_dim = 3;
+    break;
+  case 3:
+    r_on = 0;
+    state_dim = 0;
+    break;
+  }
+
+  changed_led = 1;
+  led_update();
+}
+
+// 3/4
+void dimAt75()
+{
+  static char state_dim = 0;
+  switch(state_dim){
+  case 0:
+    r_on = 1;
+    state_dim = 1;
+    break;
+  case 1:
+    r_on = 1;
+    state_dim = 2;
+    break;
+  case 2:
+    r_on = 1;
+    state_dim = 3;
+    break;
+  case 3:
+    r_on = 0;
+    state_dim = 0;
+    break;
+  }
+
+  changed_led = 1;
+  led_update();
+}
+
+//blinks red then green
+char lights_alternate()
 {
   char changed = 0;
-  static enum {R = 0, G = 1} color = G;
   
+  static enum {R = 0, G = 1} color = G;
   switch(color){
   case R:
-    changed = toggle_red();
+    changed = blink_red();
     color = G;
     break;
   case G:
@@ -69,156 +174,63 @@ char state_three()
     break;
   }
 
-  led_changed = changed;
+  changed_led = changed;
   led_update();
 }
 
-void dimAt25()
+//dims led
+char dim_lights()
 {
-  switch(state_dim){
+  switch(select_dim){
   case 0:
-    red_on = 1;
+    g_on = 0;
+    changed_led = 1;
+    led_update();
+    select_dim = 1;
     break;
   case 1:
-  case 2:
-  case 3:
-    red_on = 0;
-    break;
-  default:
-    state_dim = 0;
-    break;
-  }
-
-  led_changed = 1;
-  led_update();
-}
-
-void dimAt50()
-{
-  switch(state_dim){
-  case 0:
-  case 1:
-    red_on = 1;
-    break;
-  case 2:
-  case 3:
-    red_on = 0;
-    break;
-  default:
-    state_dim = 0;
-    break;
-  }
-
-  led_changed = 1;
-  led_update();
-}
-
-
-void dimAt75()
-{
-  switch(state_dim){
-  case 0:
-    red_on = 0;
-    break;
-  case 1:
-  case 2:
-  case 3:
-    red_on = 1;
-    break;
-  default:
-    state_dim = 0;
-    break;
-  }
-
-  led_changed = 1;
-  led_update();
-}
-
-char state_four()
-{
-  static char dim = 0;
-
-  switch(dim){
-  case 0:
     dimAt25();
-    dim = 1;
-    break;
-  case 1:
-    dimAt50();
-    dim = 2;
+    dimAt25();
+    dimAt25();
+    dimAt25();
+    select_dim = 2;
     break;
   case 2:
-    dimAt75();
-    dim = 0;
+    dimAt50();
+    dimAt50();
+    select_dim = 3;
     break;
-  default:
-    red_on = 0;
-    green_on = 0;
-    dim = 0;
+  case 3:
+    dimAt75();
+    dimAt75();
+    dimAt75();
+    dimAt75();
+    select_dim = 0;
     break;
   }
-  
   return 1;
 }
 
+//method that dictates what each button does
 void state_advance()
 {
   char changed = 0;
   switch(switch_state_changed){
-
   case 1:
-    changed = toggle_red();
+    changed = blink_red();          //button1 blinks green
     break;
   case 2:
-    changed = state_two();
+    changed = lights_alternate();   //button2 blinks red then green
     break;
   case 3:
-    changed = state_three();
+    changed = toggle_red();         //button3 blinks red and green at same time
+    changed = toggle_green();
     break;
   case 4:
-    changed = state_four();
+    changed = dim_lights();         //button4 blinks dim red light
     break;
   }
 
-  led_changed = changed;
+  changed_led = changed;
   led_update();
 }
- 
-
-/*void state_advance()
-{
-  switch(state){
-  case 0:
-    beep();
-    break;
-
-  case 1:
-    beep1();
-    redLight();
-    state = 0;
-    break;
-
-  case 2:
-    beep2();
-    dimAndBlinking();
-    state = 0;
-
-  case 3:
-    beep3();
-    redLight();
-    greenLight();
-    state = 0;
-    break;
-
-  case 4:
-    beep4();
-    redThenGreen();
-    state = 0;
-    break;
-
-  default:
-    break;
-  }
-}
-*/
- 
--- project/stateMachine.h
+++ project/stateMachine.h
@@ -2,11 +2,12 @@
 #define stateMachine_included
 
 void state_advance();
+
 void dim_leds(int x);
-
 char toggle_green();
-char toggle_red();
-
+//char toggle_red();
+char blink_red();
+//char blink_green();
 void dimAt25();
 void dimAt50();
 void dimAt75();
